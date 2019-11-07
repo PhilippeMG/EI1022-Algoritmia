34cc85b1a74a1a9aa89b286acd870c10dba6f15e
al375923 <al375923@uji.es>
2019-11-07 20:15:23 +0100

    algoritmo2 terminado y verificado

--- Entregable2/entregable2.py
+++ Entregable2/entregable2.py
@@ -3,7 +3,7 @@ import sys
 from typing import *
 
 from algoritmia.datastructures.digraphs import UndirectedGraph
-from algoritmia.datastructures.prioritymaps import heapmap
+from algoritmia.datastructures.prioritymaps import MaxHeapMap
 
 from Utils.graphcoloring2dviewer import GraphColoring2DViewer
 
@@ -27,21 +27,13 @@ def colour_vertex(lista: List[Vertex]) -> List[Set[int]]:
 
 def algoritmo1(g: UndirectedGraph) ->Tuple[int, Dict[Tuple[int,int], int]]:
     # # List of vertices
-    #------------------------------------------
-    # vertices = set(g.V)
-    # # Creating dictionary
-    # dic = {}
-    # for vertex in vertices:
-    #     # Adding successors in dictionary
-    #     dic[vertex] = len(g.succs(vertex))
-    #------------------------------------------
     dic= crear_diccionario(g)
     # Sorting dictionary
-    dic = sorted(dic.items(), key=lambda x: (x[1], x[0]), reverse=True)
+    lista_ordenada = sorted(dic.items(), key=lambda x: (x[1], x[0]), reverse=True)
 
     # Create list of sorted vertices
     lista_vertices = []
-    for tupla in dic:
+    for tupla in lista_ordenada:
         vertex = tupla[0]
         lista_vertices.append(vertex)
 
@@ -62,18 +54,56 @@ def algoritmo1(g: UndirectedGraph) ->Tuple[int, Dict[Tuple[int,int], int]]:
     diccionario_ordenado = {}
     # Creation sorted dictionary of coloured vertices
     for (k, v) in lista_ordenada:
-        #print(k[0], k[1], v)
         diccionario_ordenado[k] = v
 
     return [len(num_used_colors), diccionario_ordenado]
 
-def algoritmo2(g: UndirectedGraph):  # ->Tuple[int, Dict[Tuple[int,int], int]]:
-    #diccionario de prioridad heapsmaps
-    # Creating dictionary
-    dic =  crear_diccionario(g)
-    heap=heapmap(dic)
+def algoritmo2(g: UndirectedGraph) ->Tuple[int, Dict[Tuple[int,int], int]]:
 
-    print(heap)
+    #obtenemos una lista vertices
+    lista_vertices = g.V
+    #creamos el diccionario
+    dic = {}
+    for v in lista_vertices:
+        dic[v] = (0,len(g.succs(v)))
+    diccionario_prioridad = MaxHeapMap(dic)
+
+    #rellenamos el diccionario con los colores
+    diccionario_grupo_colores = {}
+    while len(diccionario_prioridad) > 0:
+        elem = diccionario_prioridad.extract_opt()
+        added = False
+        vecinos = g.succs(elem)
+        for i in sorted(diccionario_grupo_colores.keys()):
+            if all(u not in diccionario_grupo_colores.get(i) for u in vecinos):
+                diccionario_grupo_colores.get(i).append(elem)
+                added = True
+                break
+        #le añadimos otro color si no estan disponibles los demas
+        if added == False:
+            diccionario_grupo_colores[len(diccionario_grupo_colores.keys())] = [elem]
+
+        #actualizamos la puntuacion
+        for sucesor in vecinos:
+            if sucesor in diccionario_prioridad.keys():
+                diccionario_prioridad[sucesor] = (diccionario_prioridad[sucesor][0]+1, diccionario_prioridad[sucesor][1])
+
+    #asignamo un color para cada vertice
+
+    diccionario_soluciones = {}
+    for color in diccionario_grupo_colores.keys():
+        for elem in diccionario_grupo_colores[color]:
+            diccionario_soluciones[elem] = color
+
+
+    lista_ordenada = sorted(diccionario_soluciones.items())
+    diccionario_ordenado = {}
+    for (k, v) in lista_ordenada:
+        diccionario_ordenado[k] = v
+    return [len(diccionario_grupo_colores), diccionario_ordenado]
+
+
+    #-----------------------------------------------------------------------------
 
 def crear_diccionario(g: UndirectedGraph):
     dic = {}
@@ -115,6 +145,38 @@ def imprimir_resultado(tupla: Tuple[int, Dict[Tuple[int,int], int]]):
     print (num_colores)
     for (k, v) in dic.items():
          print (k[0],k[1], v)
+def comparar_solucion(tupla: Tuple[int, Dict[Tuple[int,int], int]],filename):
+    num_colores = tupla[0]
+    dic = tupla[1]
+    lista = []
+    for elem in dic.items():
+        lista.append(elem)
+    text_file = open(filename)
+    pos=0
+    primero=True
+    for line in text_file:
+        datos = line.rstrip().split(" ")  # vector 4 elementos
+        if primero:
+            primero=False
+            #print(num_colores, "==",datos[0] )
+            if not num_colores==int(datos[0]):
+                return False
+        else:
+            list=lista[pos]
+            vertex=list[0]
+            x=vertex[0]
+            y=vertex[1]
+            c=dic[vertex]
+
+            pos+=1
+            if( x != int(datos[0]) or y != int(datos[1]) or c != int(datos[2]) ):
+
+                return False
+    text_file.close()
+
+    return True
+
+
 
 
 if __name__ == '__main__':
@@ -130,21 +192,14 @@ if __name__ == '__main__':
 
 
         elif (sys.argv[1]) == "-2":
-            #tupla =
-            algoritmo2(g)
-            print("Algoritmo 2:\n {0}".format(tupla))
-        #imprimimos resultados
-        num_colores = tupla[0]
-        dic = tupla[1]
+            tupla = algoritmo2(g)
 
-        print(num_colores)
-        for (k, v) in dic.items():
-            print(k[0], k[1], v)
+        imprimir_resultado(tupla)
 
+        #print("Comparamos Solucion: ",comparar_solucion(tupla,"./test/graph-iberia.sol1"))
+        #print(es_coloreado_correcto(g,tupla[1]))
         if len(sys.argv) == 4 and sys.argv[3] == "-g":
-            # color_dic = {(-3, -2): 0, (0, 0): 1, (1, 1): 2}
-            viewer = GraphColoring2DViewer(g, tupla[1], window_size=(800, 800))
-            #  viewer = Graph2dViewer(g, window_size=(400, 200))
+            viewer = GraphColoring2DViewer(g, tupla[1], window_size=(1000, 1000))
             viewer.run()
 
     else:
